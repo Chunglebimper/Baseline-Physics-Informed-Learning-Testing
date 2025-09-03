23f0e24ffe6c63e66a9926d276542bc0e32f2f14
Chunglebimper <chunglebimper@protonmail.com>
2025-09-03 13:55:37 -0400

    Added percent for random number generator; WIP

--- src/dataset.py
+++ src/dataset.py
@@ -7,13 +7,15 @@ from torchvision import transforms
 
 # Dataset class to handle pre, post, and mask images
 class DamageDataset(Dataset):
-    def __init__(self, pre_dir, post_dir, mask_dir, patch_size=128, stride=64, mode='post'):
+    def __init__(self, pre_dir, post_dir, mask_dir, patch_size=128, stride=64, mode='post', balance = False, oversample=2):
         self.pre_dir = pre_dir
         self.post_dir = post_dir
         self.mask_dir = mask_dir
         self.patch_size = patch_size
         self.stride = stride
         self.mode = mode
+        self.balance = balance
+        self.oversample = oversample
 
         # Standard transforms
         self.base_transform = transforms.Compose([
@@ -28,10 +30,12 @@ class DamageDataset(Dataset):
         ])
 
         # Collect image samples
+        # Collect image samples
         self.filenames = sorted([f for f in os.listdir(mask_dir) if f.endswith(f"_{mode}_disaster_target.png")])
         self.samples = []
-        patches_featuring_class = {'class0':0, 'class1':0, 'class2':0, 'class3':0, 'class4':0}
-        print("Patches featuring class 4:")
+        patches_featuring_class = {'class0': 0, 'class1': 0, 'class2': 0, 'class3': 0, 'class4': 0}
+
+        # first, count the patches with each class
         for fname in self.filenames:
             basename = fname.replace(f"_{mode}_disaster_target.png", "")
             mask = np.array(Image.open(os.path.join(self.mask_dir, fname)).convert('L'))
@@ -39,18 +43,13 @@ class DamageDataset(Dataset):
             for y in range(0, h - patch_size + 1, stride):
                 for x in range(0, w - patch_size + 1, stride):
                     patch = mask[y:y + patch_size, x:x + patch_size]
-
-                    # downsampling
-                    include = (4 in patch or 3 in patch or 2 in patch or np.random.rand() < 0.1)
-
-
-                    if include:
-                        is_priority = any(cls in patch for cls in [2, 3, 4])
-                        print((f'\t{basename, x, y}\n') if 4 in patch else "", end="")
-                        self.samples.append((basename, x, y, is_priority))
-
+                    #include = (4 in patch or 3 in patch or 2 in patch or np.random.rand() < 0.1)
+                    #if include:
+                    #    is_priority = any(cls in patch for cls in [2, 3, 4])
+                    #    self.samples.append((basename, x, y, is_priority))
+                    if True:
+                        ###############################################################################################
                         has_c0, has_c1, has_c2, has_c3, has_c4 = (c in patch for c in [0, 1, 2, 3, 4])  # boolean
-
                         if has_c0:
                             patches_featuring_class[f'class0'] += 1
                         if has_c1:
@@ -62,12 +61,18 @@ class DamageDataset(Dataset):
                         if has_c4:
                             patches_featuring_class[f'class4'] += 1
 
-
-        print(f"{25 * '-'} Undersampling Complete {25 * '-'}")
-        print("Number of patches featuring classes:")
         for key, value in patches_featuring_class.items():
             print(f'\t{key} : {value}')
-
+        print("-------------- All Samples Loaded --------------\n"
+              "With oversampling/undersampling take this percentage of each class:\n")
+        percent2include = {'class0': (oversample * patches_featuring_class[f'class4'] ),
+                           'class1': (oversample * patches_featuring_class[f'class4']),
+                           'class2': (oversample * patches_featuring_class[f'class4']),
+                           'class3': (oversample * patches_featuring_class[f'class4']),
+                           'class4': (patches_featuring_class[f'class4'] / patches_featuring_class[f'class0'])
+                           }
+        print(percent2include)
+        ###############################################################################################
         """
         We are removing class 0 by 90%
         We need to oversmaple minority
@@ -75,7 +80,7 @@ class DamageDataset(Dataset):
         - Oversample number of patches and undersample majority
             - to do this take a random patch and make sure the name doesnt conflict (change 19th character)
             / option 1: load all samples; then go through samples randomly until quota is filled
-            / option 2: as samples are loading; duplicate the samplesas you are going
+            / option 2: as samples are loading; duplicate the samples as you are going
         """
 
     def oversampling(self, class0=1, class1=1, class2=1, class3=1, class4=1):
