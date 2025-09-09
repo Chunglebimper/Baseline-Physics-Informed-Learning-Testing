8f2535077d5cfe03888e6c9133db5ec3365a6497
Chunglebimper <chunglebimper@protonmail.com>
2025-09-08 20:55:56 -0400

    Running model; TODO: reuslt organization

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 # below are the baseline runs
-python main.py --use_glcm --batch_size 2 --patch_size 64 --stride 32 --epochs 31 --data_root ../data --oversample 10 
+#python main.py --use_glcm --batch_size 2 --patch_size 64 --stride 32 --epochs 31 --data_root ../data --oversample 10 
+
+for i in 32 64
+do
+    for j in 4 8 16 
+    do 
+        python main.py --use_glcm --batch_size 2 --patch_size $i --stride $j --epochs 50 --data_root ../data --oversample 10 
+    done
+done
+
+
